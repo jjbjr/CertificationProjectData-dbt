dfd7f54a67ecdb1ca99e6fab1c4d2cd2c28a321b
jurandirjbjr@gmail.com <jurandirjbjr@gmail.com>
2023-06-16 15:15:44 +0000

    salvando progresso stg territorio vendas

---
+++ models/staging/sap/stg_sap_territoriovendas.sql
@@ -0,0 +1,21 @@
+with
+    fonte_territoriovendas as (
+        select *                
+        from {{ source('sap', 'salesterritory') }}
+    )
+    , renomear as (
+        select
+            cast(territoryid as integer) as pk_id_territorio				
+            , cast(name as string) as territorio					
+            , cast(countryregioncode as string) as sigla					
+            , cast (`group` as string) as regiao					
+            --, salesytd				
+            --, saleslastyear				
+            --, costytd				
+            --, costlastyear				
+            , cast (rowguid as string) as rowguid_territoriovendas					
+            , cast (modifieddate as string) as data_modificada_territoriovendas		               
+        from fonte_territoriovendas
+    )
+select *
+from renomear
