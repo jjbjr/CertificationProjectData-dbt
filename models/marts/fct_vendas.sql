2af8bcc3001dc2b128683909cbc99f6e13d74370
jurandirjbjr@gmail.com <jurandirjbjr@gmail.com>
2023-06-25 13:25:20 +0000

    Correções fct vendas

--- models/marts/fct_vendas.sql
+++ models/marts/fct_vendas.sql
@@ -5,85 +5,51 @@ with
     )
 
     , clientes as (
-        select *
+        select 
+            sk_cliente
+            , pk_id_cliente
         from {{ ref('dim_clientes') }}
     )
 
     , produtos as (
-        select *
+        select 
+            sk_produto
+            , pk_id_produto
         from {{ ref('dim_produtos') }}
     )
 
     , join_tabelas as (
         select
-            pedidos.id_pedido	
-            --, pedidos.sk_pedidos as fk_pedido
-            , clientes.sk_cliente as fk_cliente
-            , produtos.sk_produto as fk_produto
-	
-            --, pedidos.id_detalhepedido				
-            --, pedidos.id_produto
-            --, pedidos.id_cliente				
-            --, pedidos.id_entidadenegocio					
-            --, pedidos.id_territorio					
-            --, pedidos.id_endereco		
-            --, pedidos.id_cartao
-            --, produtos.pk_id_produto
-            --, produtos.pk_id_modeloproduto
-            --, produtos.pk_id_subcategoria
-            --, produtos.pk_id_categoria	
-            --, clientes.pk_id_entidadenegocio
-            --, clientes.pk_id_cliente
-            --, clientes.pk_id_cartao 
-            --, clientes.pk_id_endereco	
-            --, clientes.pk_id_estado
-
-            --, pedidos.motivovenda
+            pedidos.id_pedido as pk_pedido
+            , sk_cliente as fk_cliente
+            , sk_produto as fk_produto
             , pedidos.data_pedido
             , pedidos.quantidade		
             , pedidos.precounitario					
             , pedidos.desconto				
-            --, pedidos.revisao_pedido		
             , pedidos.data_envio					
             , pedidos.status_pedido					
             , pedidos.ordem_compra					
             , pedidos.numero_conta_financeiro	
             , pedidos.motivo
-
-            , clientes.cliente
-            , clientes.tipo_pessoa
-            , clientes.cidade					
-            , clientes.sigla_estado					
-            , clientes.estado	
-            , clientes.sigla_pais					
-            , clientes.pais
-            , clientes.bandeira_cartao       
-            , clientes.numero_cartao                 
-            , clientes.mes_validade                  
-            , clientes.ano_validade
+	
             
-            , produtos.categoria
-            , produtos.subcategoria	
-            , produtos.modelo 	
-            , produtos.produto						
-            , produtos.numero_produto						
-            --, produtos.preco_custo					
-            --, produtos.preco_venda		
-            
-
         from pedidos
         left join clientes on pedidos.id_cliente = clientes.pk_id_cliente
         left join  produtos on pedidos.id_produto = produtos.pk_id_produto
 
     )
 
-    --, transformacoes as (
-    --    select
-    --    from
-    --)
+    , transformacoes as (
+        select 
+            {{ dbt_utils.generate_surrogate_key(['pk_pedido', 'fk_produto']) }} as sk_venda
+            , *
+
+        from join_tabelas
+    )
 
 select *
-from join_tabelas
+from transformacoes
 --limit 131933
 /* Revisar todas as tabelas pois o preco sugerido de venda esta muito direferente do preco 
 na venda mesmo aplicando o desconto*/
